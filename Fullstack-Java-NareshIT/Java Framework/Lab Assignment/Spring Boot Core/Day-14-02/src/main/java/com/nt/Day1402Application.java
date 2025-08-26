c526736d368cd7820adc5034231bb3675b7cbd01
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-08-27 00:05:30 +0530

    Update

---
+++ Fullstack-Java-NareshIT/Java Framework/Lab Assignment/Spring Boot Core/Day-14-02/src/main/java/com/nt/Day1402Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1402Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1402Application.class, args);
+	}
+
+}
